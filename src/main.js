e2d9a14a79f5b6110b07c12755a0092bfa789c11
Bohdan Moskalevskyi <msklvsk@icloud.com>
2019-12-08 22:00:31 +0200

    add repro demo

--- src/main.js
+++ src/main.js
@@ -5,4 +5,6 @@ Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  // eslint-disable-next-line no-console
+  mounted: () => console.log(document.getElementsByTagName('p')[0].innerHTML.charCodeAt(1))
 }).$mount('#app')
